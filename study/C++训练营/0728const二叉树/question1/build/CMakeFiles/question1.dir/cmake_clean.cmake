3a3ea27f698a59579c7f3e374fdc868a95f4c380
wdb-ws <1035059681@qq.com>
2025-07-28 21:25:50 +0800

    [add] study cpp

---
+++ study/C++训练营/0728const二叉树/question1/build/CMakeFiles/question1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/question1.dir/q3.cpp.o"
+  "CMakeFiles/question1.dir/q3.cpp.o.d"
+  "question1"
+  "question1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/question1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
